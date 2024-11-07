916f429539d2f8164b418329830d2b13a6f0f7f5
Matthew R. Ochs <mochs@nvidia.com>
2024-11-07 15:55:58 -0800

    NVIDIA: SAUCE: vfio/nvgrace-gpu: Handle EGM registration failure
    BugLink: https://bugs.launchpad.net/bugs/2119656
    
    Detect and handle a failure from the EGM registration service.
    
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Kai-Heng Feng <kaihengf@nvidia.com>
    Acked-by: Carol L. Soto <csoto@nvidia.com>
    Acked-by: Koba Ko <kobak@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    (cherry picked from commit f18eee3bbdea77a9b525c0665d7ebe1992bb00b2 https://github.com/NVIDIA/NV-Kernels/tree/24.04_linux-nvidia-adv-6.8-next)
    Signed-off-by: Koba Ko <kobak@nvidia.com>
    Acked-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Carol L. Soto <csoto@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    (cherry picked from commit 8371b68c33cc03a7ea6dfd7bdfc0fe9d47ec64fb https://github.com/NVIDIA/NV-Kernels/tree/24.04_linux-nvidia-adv-6.11-next)
    Signed-off-by: Nirmoy Das <nirmoyd@nvidia.com>
    Acked-by: Carol L Soto <csoto@nvidia.com>
    Acked-by: Matt Ochs <mochs@nvidia.com>
    Acked-by: Noah Wager <noah.wager@canonical.com>
    Acked-by: Jacob Martin <jacob.martin@canonical.com>
    Signed-off--by: Brad Figg <bfigg@nvidia.com>
    
    (cherry picked from commit be5ae8ffa6ef noble:linux-nvidia-6.14)
    Signed-off-by: Abdur Rahman <abdur.rahman@canonical.com>

--- drivers/vfio/pci/nvgrace-gpu/main.c
+++ drivers/vfio/pci/nvgrace-gpu/main.c
@@ -989,7 +989,10 @@ static int nvgrace_gpu_probe(struct pci_dev *pdev,
 			goto out_put_vdev;
 
 		if (egm_enabled) {
-			register_egm_node(pdev);
+			ret = register_egm_node(pdev);
+			if (ret)
+				goto out_put_vdev;
+
 			nvdev->egm_node = egmpxm;
 		}
 
@@ -997,10 +1000,13 @@ static int nvgrace_gpu_probe(struct pci_dev *pdev,
 
 	ret = vfio_pci_core_register_device(&nvdev->core_device);
 	if (ret)
-		goto out_put_vdev;
+		goto out_egm_unreg;
 
 	return ret;
 
+out_egm_unreg:
+	if (egm_enabled)
+		unregister_egm_node(nvdev->egm_node);
 out_put_vdev:
 	vfio_put_device(&nvdev->core_device.vdev);
 	return ret;
